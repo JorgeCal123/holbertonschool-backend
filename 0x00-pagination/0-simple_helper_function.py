1c369cbd22fad823769d2e5e546637f14a6513e2
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-22 12:09:36 -0500

    task 0 corr 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """
-    function named index_range that takes two 
+    function named index_range that takes two
     integer arguments page and page_size
     """
     size_index = page * page_size
