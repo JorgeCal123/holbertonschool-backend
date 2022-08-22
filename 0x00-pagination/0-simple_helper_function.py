36e2fefe31727d294ee807f8bdf4179099267ac1
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-22 11:59:50 -0500

    all task

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Simple Helper Function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    function named index_range that takes two 
+    integer arguments page and page_size
+    """
+    size_index = page * page_size
+    index = size_index - page_size
+    return (index, size_index)
