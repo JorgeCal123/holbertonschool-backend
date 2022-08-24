d8b4b67a4666fecce1ff89af75e1d680f55da56e
JorgeCal123 <ingsistemas68@gmail.com>
2022-08-24 10:43:03 -0500

    task 0 corr 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -24,14 +24,9 @@ class BaseCaching():
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None or item is not None:
-            self.cache_data[key] = item
-        #raise NotImplementedError("put must be implemented in your cache class")
+        raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
-        #raise NotImplementedError("get must be implemented in your cache class")
+        raise NotImplementedError("get must be implemented in your cache class")
